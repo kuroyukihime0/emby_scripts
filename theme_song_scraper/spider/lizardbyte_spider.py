8358503be915358ada8d4c6b52efa23cd29d53ba
kuroyukihime0 <7975549+kuroyukihime0@users.noreply.github.com>
2024-02-06 11:31:58 +0800

    update spider

--- theme_song_scraper/spider/lizardbyte_spider.py
+++ theme_song_scraper/spider/lizardbyte_spider.py
@@ -23,13 +23,18 @@ def save_to_json():
 
 
 
-def get_json(url):
-    response = session.get(url)
-    if response.status_code == 200:
-        resp_json = response.json()
-        return resp_json
-    else:
-        return None
+def get_json(url,retry = 0):
+    try:
+        response = session.get(url)
+        if response.status_code == 200:
+            resp_json = response.json()
+            return resp_json
+        else:
+            return None
+    except Exception as ex:
+        print(str(ex))
+        if(retry <=3):
+            return get_json(url,retry +1)
 
 def get_page_count():
     return get_json("https://app.lizardbyte.dev/ThemerrDB/movies/pages.json")
@@ -37,6 +42,11 @@ def get_page_count():
 def get_page_detail(page):
     return get_json( f"https://app.lizardbyte.dev/ThemerrDB/movies/all_page_{page}.json")
 
+def get_item_theme(url):
+    resp_json = get_json(url)
+    if not resp_json or "youtube_theme_url" not in resp_json:
+        return None
+    return resp_json["youtube_theme_url"]
 
 page_count = get_page_count()
 if page_count:
@@ -47,7 +57,15 @@ if page_count:
             if item in items:
                 pass
             else:
+                url = None
+                if 'id' in item:
+                    url = f"https://app.lizardbyte.dev/ThemerrDB/movies/themoviedb/{item['id']}.json"
+                elif 'imdb_id' in item:
+                    url = f"https://app.lizardbyte.dev/ThemerrDB/movies/imdb/{item['imdb_id']}.json"
+                if url:
+                    item['theme'] = get_item_theme(url)
                 items.append(item)
                 save_to_json()
 else:
     pass
+print('all done')
